5d648c59f4251119161a77d905d6dc141d31efd2
ZixuanShen <zxshenmonica@gmail.com>
2024-01-18 17:30:12 +0800

    Lab2 IntList Updated

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,14 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A == null)
+            return B;
+        IntList lastA = A;
+        while(lastA.rest != null){
+            lastA = lastA.rest;
+        }
+        lastA.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +98,9 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A == null)
+            return B;
+        return new IntList(A.first, catenate(A.rest, B));
     }
 
 
